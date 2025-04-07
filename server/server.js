b326c94a47521fc33a2a9e96687cd2fe7cd6b1c3
heroji629 <instaji789@gmail.com>
2025-04-07 12:16:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const PORT = 5000;
+const connectDB = require('./config/database');
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Database Connection
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Asatu Backend Running');
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Database: ${process.env.MONGODB_URI.split('@')[1]}`); // Shows cluster info
 });

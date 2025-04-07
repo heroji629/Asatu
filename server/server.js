6ef29b29131a25781ba8fa95269b5e78b426f2e2
heroji629 <instaji789@gmail.com>
2025-04-07 12:30:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 // Database Connection
 connectDB();
+// Routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
 // Middleware
 app.use(express.json());
